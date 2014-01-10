e37dc20c1fc645f64b8fd6e6afdf57192cae0cfe
Emmanuel Fleury <emmanuel.fleury@labri.fr>
2014-01-10 15:20:17 +0100

    Setting properly the localisation

--- hackingweek/settings.py
+++ hackingweek/settings.py
@@ -31,7 +31,7 @@ TIME_ZONE = "Europe/Paris"
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = "fr_FR"
+LANGUAGE_CODE = "fr"
 
 LOCALE_PATHS = (
     'locale/'
