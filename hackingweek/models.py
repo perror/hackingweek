b200211c365f93378be3e6babdf1bea3e74764dc
Emmanuel Fleury <emmanuel.fleury@labri.fr>
2014-01-12 20:47:07 +0100

    Polishing a bit before starting team creation

--- hackingweek/models.py
+++ hackingweek/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Contest(models.Model):
+    max_team_members = 5
+    begin_date = ''
+    end_date = ''
 
 class Challenge(models.Model):
     name   = models.CharField(max_length=128)
@@ -8,14 +12,12 @@ class Challenge(models.Model):
     description = models.CharField(max_length=2048)
     password = models.CharField(max_length=128)
 
-
 class Team(models.Model):
     max_members = 5
     name = models.CharField(max_length=128)
     members = models.ManyToManyField(User,
                                      related_name='team_list',
                                      null=True, blank=True)
-
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
