f8542940b4d08a2e965d7038ca17c5e6cc37fc26
Emmanuel Fleury <emmanuel.fleury@labri.fr>
2014-02-03 00:22:19 +0100

    Fixing the ordering once for all

--- hackingweek/templatetags/hackingweek_tags.py
+++ hackingweek/templatetags/hackingweek_tags.py
@@ -4,19 +4,17 @@ from django.core.urlresolvers import reverse
 
 from hackingweek.models import UserProfile, Team
 
+from django.template.base import Variable, VariableDoesNotExist
 
 register = template.Library()
-
 
 @register.simple_tag
 def user_count():
     return User.objects.exclude(is_staff=True).count()
 
-
 @register.simple_tag
 def team_count():
     return Team.objects.all().count()
-
 
 @register.simple_tag
 def active(request, pattern):
@@ -24,4 +22,13 @@ def active(request, pattern):
     if re.search(reverse(pattern), request):
         return ' class="active"'
     else:
+        return ''
+
+@register.filter
+def sort(value, arg):
+    try:
+        return sorted(value,
+                      key=Variable(arg).resolve,
+                      cmp=lambda x,y: cmp(x.lower(), y.lower()))
+    except (TypeError, VariableDoesNotExist):
         return ''
