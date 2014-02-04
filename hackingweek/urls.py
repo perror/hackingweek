579f9098456fac261bfe604111f3dff5f584e0d6
Emmanuel Fleury <emmanuel.fleury@labri.fr>
2014-02-04 22:51:19 +0100

    Adding the contest rules page

--- hackingweek/urls.py
+++ hackingweek/urls.py
@@ -18,6 +18,7 @@ from hackingweek.decorators import has_no_team_required, has_team_required
 urlpatterns = patterns("",
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r"^about/$", TemplateView.as_view(template_name="about.html"), name="about"),
+    url(r"^rules/$", TemplateView.as_view(template_name="rules.html"), name="rules"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/settings/$", hackingweek.views.SettingsView.as_view(), name="account_settings"),
     url(r"^account/signup/$", hackingweek.views.SignupView.as_view(), name="account_signup"),
