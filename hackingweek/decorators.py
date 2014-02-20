6a8753bcc8f1090e93b1f690bead4fda8e810d76
Arthur Darcet <arthur.darcet@m4x.org>
2014-02-20 18:23:48 +0100

    Allow users with more than one team to quit a team

--- hackingweek/decorators.py
+++ hackingweek/decorators.py
@@ -8,7 +8,7 @@ def has_no_team_required(view_func):
     decorated_view_func = login_required(has_no_team(view_func))
     return decorated_view_func
 
-has_team = user_passes_test(lambda u: u.team_set.all().count() == 1 or u.is_staff)
+has_team = user_passes_test(lambda u: u.team_set.all().count() >= 1 or u.is_staff)
 
 def has_team_required(view_func):
     decorated_view_func = login_required(has_team(view_func))
