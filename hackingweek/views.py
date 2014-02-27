dbcb11582d65a611612e5a60d26d6fdf4182ab01
Emmanuel Fleury <emmanuel.fleury@labri.fr>
2014-02-27 14:58:22 +0100

    Fixing a stupid bug that appeared when trying to merge

--- hackingweek/views.py
+++ hackingweek/views.py
@@ -50,10 +50,6 @@ def validate(request, pk):
          },
       }
 
-   # Check for the last attempt timestamp
-   now = datetime.datetime.now()
-
-   
    # Check if the contest is open
    start_date = \
        datetime.strptime(settings.CONTEST_START_DATE, "%Y-%m-%d %H:%M")
