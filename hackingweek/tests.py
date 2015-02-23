bf5ce922f60b981a08ae9f3db4853b94b1088990
Emmanuel Fleury <emmanuel.fleury@u-bordeaux.fr>
2015-02-23 20:15:33 +0100

    Adding a test for exception raising

--- hackingweek/tests.py
+++ hackingweek/tests.py
@@ -1,9 +1,21 @@
 from django.test import TestCase
 
-from hackingweek.models import Challenge
+from hackingweek.models import Category, Challenge
+
+def wrong_challenge():
+    chall = Challenge()
+    chall.category = 'a'
 
 class ChallengeMethodTests(TestCase):
 
     def test_unknown_category(self):
-        mychall = Challenge()
-        self.assertRaises(ValueError, mychallcategory = 'a')
+        self.assertRaises(ValueError, wrong_challenge)
+
+    def test_Challenge_creation(self):
+        chall = Challenge()
+        mycat = Category('my category')
+        chall.category = mycat
+        chall.name = 'my challenge'
+        chall.author = 'anonymous'
+        chall.body = 'No comment'
+        chall.key = '12345'
